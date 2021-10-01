1897f60c66b42eed5947cb8b920b6dc1b78d80e4
0xb4dc0d3 <69420353+0xb4dc0d3x@users.noreply.github.com>
2021-10-01 21:06:12 +0330

    Adding Items

---
+++ src/presence/presence.py
@@ -0,0 +1,65 @@
+from pypresence import Presence as PyPresence
+from pypresence.exceptions import InvalidPipe
+from InquirerPy.utils import color_print
+import time, sys, traceback, os, ctypes
+
+from ..utilities.config.app_config import Config
+from ..content.content_loader import Loader
+from ..localization.localization import Localizer
+from .presences import (ingame,menu,startup,pregame)
+
+kernel32 = ctypes.WinDLL('kernel32')
+user32 = ctypes.WinDLL('user32')
+hWnd = kernel32.GetConsoleWindow()
+
+class Presence:
+
+    def __init__(self,config):
+        self.config = config
+        self.client = None
+        self.saved_locale = None
+        try:
+            self.rpc = PyPresence(client_id=str(Localizer.get_config_value("client_id")))
+            self.rpc.connect()
+        except InvalidPipe as e:
+            raise Exception(e)
+        self.content_data = {}
+    
+    def main_loop(self):
+        try:
+            self.content_data = Loader.load_all_content(self.client)
+            color_print([("LimeGreen bold", Localizer.get_localized_text("prints","presence","presence_running"))])
+            while True:
+                presence_data = self.client.fetch_presence()
+                if presence_data is not None:
+                    self.update_presence(presence_data["sessionLoopState"],presence_data)
+                    # print(presence_data)
+                else:
+                    os._exit(1)
+
+                if Localizer.locale != self.saved_locale:
+                    self.saved_locale = Localizer.locale
+                    self.content_data = Loader.load_all_content(self.client)
+
+                time.sleep(Localizer.get_config_value("presence_refresh_interval"))
+
+                
+
+                    
+        except Exception as e:
+            user32.ShowWindow(hWnd, 1)
+            kernel32.SetConsoleMode(kernel32.GetStdHandle(-10), (0x4|0x80|0x20|0x2|0x10|0x1|0x40|0x100))
+            color_print([("Red bold",Localizer.get_localized_text("prints","errors","error_message"))])
+            traceback.print_exc()
+            input(Localizer.get_localized_text("prints","errors","exit"))
+            os._exit(1)
+
+    def update_presence(self,ptype,data=None):
+        presence_types = {
+            "startup": startup,
+            "MENUS": menu,
+            "PREGAME": pregame,
+            "INGAME": ingame,
+        }
+        if ptype in presence_types.keys():
+            presence_types[ptype].presence(self.rpc,client=self.client,data=data,content_data=self.content_data,config=self.config)
---
+++ src/presence/presences/menu_presences/away.py
@@ -0,0 +1,11 @@
+from ....localization.localization import Localizer
+from ...presence_utilities import Utilities
+
+def presence(rpc,client=None,data=None,content_data=None,config=None):
+    _, mode_name = Utilities.fetch_mode_data(data,content_data)
+    rpc.update(
+        details=f"{mode_name}",
+        state=f"{Localizer.get_localized_text('presences','client_states','away')}",
+        large_image="game_icon_yellow",
+        large_text="VALORANT",
+    )
