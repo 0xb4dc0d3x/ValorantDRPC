c925bf9dd97d27e784cde59ead89ee78a14d2221
0xb4dc0d3x <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>
2022-06-07 01:33:25 +0430

    fade added to program

--- src/utilities/config/app_config.py
+++ src/utilities/config/app_config.py
@@ -8,7 +8,7 @@ from ...localization.locales import Locales
 from ...localization.localization import Localizer
 
 default_config = {
-    "version": "v1.1.0",
+    "version": "v1.2.0",
     "region": ["", Client.fetch_regions()],
     "client_id": 811469787657928704,
     "presence_refresh_interval": 3,
